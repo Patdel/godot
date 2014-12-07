9884a919be43f305317307ce556fddd2d37119b3
Patricio Calderon <ourobor@gmail.com>
2014-12-07 00:22:10 -0500

    creates loop to respond purse commnand

--- w01/d05/student/ShylockBot.rb
+++ w01/d05/student/ShylockBot.rb
@@ -1,10 +1,31 @@
 
 
-def user_command
+def asking (question)
+  $stdout.puts(question)
   $stdout.print('>> ')
   $stdin.gets.chomp
 end
 
-ShylockBot_coffer = 10000
+#def double_colon (actions)
 
-ShylockBot_responses = {"purse" => "ShylockBot pulls out his purse", "loan" => "ShylockBot gives #{name} #{money} ducats", "ledger" => "ShylockBot pulls out his ledger"}
+#ShylockBot_responses = {"purse" => "ShylockBot pulls out his purse", "loan" => "ShylockBot gives #{name} #{money} ducats", "ledger" => "ShylockBot pulls out his ledger"}
+
+
+ShylockBot_purse = 10000
+
+
+while ShylockBot_purse > 0
+   action_to_perform = asking ("What would you like me to do? purse/loan/ledger")
+
+   if action_to_perform == "purse"
+    $stdout.puts ("::ShylockBot pulls out his purse::")
+    $stdout.puts (ShylockBot_purse.to_s + " ducats")
+  else
+    $stdout.puts ("::ShylockBot looks confused::")
+    break
+  end
+
+end
+
+
+
