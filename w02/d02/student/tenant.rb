98c4da9b955136ee90955da68da87cc6d80b84a2
Patricio Calderon <ourobor@gmail.com>
2014-12-09 18:36:17 -0500

    adds word initialize to tenant-class method

--- w02/d02/student/tenant.rb
+++ w02/d02/student/tenant.rb
@@ -3,7 +3,7 @@ class Tenant
 
   attr_accessor :f_name, :l_name, :born_on, :gender, :nickname, :occupation
 
-def (f_name, l_name, born_on, gender, nickname, occupation)
+def initialize(f_name, l_name, born_on, gender, nickname, occupation)
   @f_name = f_name
   @l_name = l_name
   @born_on = born_on
