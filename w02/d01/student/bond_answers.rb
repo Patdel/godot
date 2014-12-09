0a4351907f260ddaffdc2d447a814de89199d57a
Patricio Calderon <ourobor@gmail.com>
2014-12-08 21:13:47 -0500

    adds block to create an array of actors's names

--- w02/d01/student/bond_answers.rb
+++ w02/d01/student/bond_answers.rb
@@ -1,18 +1,18 @@
 
 gross_per_movie = []
 
-BOND_DATA.each {|income| gross_per_movie.push(income[:gross])}
+BOND_DATA.each {|income| gross_per_movie.push(income[:gross].gsub("$", "").gsub(",", "").to_i)}
 
-gross_per_movie[0].gsub("$", "").gsub(",", "").to_i
-
-
-BOND_DATA.each {|income| gross_per_movie.gsub("$", "").gsub(",", "").to_i.push(income[:gross])}
+sum = 0
+gross_per_movie.each {|total| sum+=total}
 
 #--------------------------------------------
 
 actors_names = []
 
-BOND_DATA.each {|names| actors_names.push(names[:actor]!)}
+BOND_DATA.each {|names| actors_names.push(names[:actor])}
+
+$stdout.puts(actors_names.uniq!)
 
 #-----------------------------------------------
 
@@ -20,4 +20,15 @@ bond_movies = []
 
 BOND_DATA.each {|movie| bond_movies.push(movie[:title])}
 
+#----------------------------------------------
+
+odd_year_titles = []
+
+BOND_DATA.each {|odd| odd_year_titles.push(odd[:year])}
+
+#----------------------------------------------
+
+worst_grossing_bond = []
+
+BOND_DATA.each {|worst| }
 
