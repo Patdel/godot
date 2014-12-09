b98b52a798ee281f185931a1720559cfc7d9469b
Patricio Calderon <ourobor@gmail.com>
2014-12-08 20:40:29 -0500

    creates blocks for actors's names and movie titles

--- w02/d01/student/bond_answers.rb
+++ w02/d01/student/bond_answers.rb
@@ -1,2 +1,23 @@
 
 gross_per_movie = []
+
+BOND_DATA.each {|income| gross_per_movie.push(income[:gross])}
+
+gross_per_movie[0].gsub("$", "").gsub(",", "").to_i
+
+
+BOND_DATA.each {|income| gross_per_movie.gsub("$", "").gsub(",", "").to_i.push(income[:gross])}
+
+#--------------------------------------------
+
+actors_names = []
+
+BOND_DATA.each {|names| actors_names.push(names[:actor]!)}
+
+#-----------------------------------------------
+
+bond_movies = []
+
+BOND_DATA.each {|movie| bond_movies.push(movie[:title])}
+
+
