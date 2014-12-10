8a99888f40e6e6987009f5d149ea6539df6a418e
Patricio Calderon <ourobor@gmail.com>
2014-12-09 19:34:13 -0500

    creates apartment class

--- w02/d02/student/apartment.rb
+++ w02/d02/student/apartment.rb
@@ -1,15 +1,17 @@
 
 class Apartment
 
+  attr_accessor :unit, :num_beds, :num_baths
 
-
-  def initialize(unit, num_beds, num_baths, tenants)
+  def initialize(unit, num_beds, num_baths)
     @unit = unit
     @num_beds = num_beds
     @num_baths = num_baths
-    @tenants = tenants
-
-
+    @tenants = []
   end
 
+a1 = Apartment.new("M", 4, 2)
+
 end
+
+
