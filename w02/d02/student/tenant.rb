c47a24fba00f24775833023840c832ac4afc2da1
Patricio Calderon <ourobor@gmail.com>
2014-12-09 19:17:53 -0500

    creates a full name method

--- w02/d02/student/tenant.rb
+++ w02/d02/student/tenant.rb
@@ -13,4 +13,14 @@ def initialize(f_name, l_name, born_on, gender, nickname, occupation)
 
 end
 
+def full_name
+  "#{@f_name} #{@nickname} #{@l_name} "
+
 end
+
+
+end
+
+guy_1=Tenant.new("Vlad", "Tepes", "1431", "Male", "The Impaler", "Prince")
+guy_1.full_name
+"Hey, that is #{}"
