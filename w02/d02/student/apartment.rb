76216bf9d43f066dd7bc2cfb3a1273533c20efc9
Patricio Calderon <ourobor@gmail.com>
2014-12-09 19:26:06 -0500

    creates apartment class

---
+++ w02/d02/student/apartment.rb
@@ -0,0 +1,15 @@
+
+class Apartment
+
+
+
+  def initialize(unit, num_beds, num_baths, tenants)
+    @unit = unit
+    @num_beds = num_beds
+    @num_baths = num_baths
+    @tenants = tenants
+
+
+  end
+
+end
