94b2c6be45b5e7332a3deba4ceaf54ced18b8e6c
Patricio Calderon <ourobor@gmail.com>
2014-12-10 20:52:46 -0500

    creates file person and then Person class

--- w02/d03/student/movie.rb
+++ w02/d03/student/movie.rb
@@ -4,12 +4,14 @@ class Movie
 
 attr_accessor :title, :director
 
-  def initialize(title)
+  def initialize(title, director)
     @title = title
+    @director = director
   end
 
 
 end
 
-movie = Movie.new("Jaws")
-movie.director
+movie = Movie.new("Jaws", "Spielberg")
+#movie.director
+
---
+++ w02/d03/student/person.rb
@@ -0,0 +1,11 @@
+
+class Person
+
+   def initialize(name, age)
+
+    @name = name
+    @age = age
+
+   end
+
+end
