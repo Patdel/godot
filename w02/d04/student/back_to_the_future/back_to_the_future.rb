5ce541d171ae2a7bc7260a75193abec1068126e9
Patricio Calderon <ourobor@gmail.com>
2014-12-11 18:20:00 -0500

    creates vehicle class, initialize method and immutable methods

---
+++ w02/d04/student/back_to_the_future/back_to_the_future.rb
@@ -0,0 +1,8 @@
+
+require pry
+
+require_relative 'modules/teenageable'
+require_relative 'modules/ninjable'
+require_relative 'modules/mutatable'
+require_relative 'classes/human'
+require_relative 'classes/turtle'
---
+++ w02/d04/student/back_to_the_future/vehicle.rb
@@ -0,0 +1,18 @@
+
+class Vehicle
+
+attr_reader :description :capacity :location
+
+
+def initialize(description, capacity, location, passengers)
+  @description = description
+  @capacity = capacity
+  @location = location
+  @passengers = passengers
+end
+
+
+
+
+
+end
