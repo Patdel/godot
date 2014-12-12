e9e228e4e8ee03b6441dc5d9c91247bf8dfe7583
Patricio Calderon <ourobor@gmail.com>
2014-12-11 19:18:22 -0500

    defines add_passenger method and tests it

--- w02/d04/student/back_to_the_future/vehicle.rb
+++ w02/d04/student/back_to_the_future/vehicle.rb
@@ -1,18 +1,22 @@
 
 class Vehicle
 
-attr_reader :description :capacity :location
+  attr_reader :description, :capacity, :location
+  attr_accessor :passengers
 
 
-def initialize(description, capacity, location, passengers)
-  @description = description
-  @capacity = capacity
-  @location = location
-  @passengers = passengers
-end
+  def initialize(description, capacity, location)
+    @description = description
+    @capacity = capacity
+    @location = location
+    @passengers = []
+  end
+
+  def add_passenger(passenger_name)
+
+    @passengers.push(passenger_name)
+
+  end
 
 
-
-
-
-end
+end #Vehicle
