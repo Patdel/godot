724670da16308b5687fc26a4f0bab892b2298260
Patricio Calderon <ourobor@gmail.com>
2014-12-18 20:31:05 -0500

    adds get request to /about

--- w03/d04/student/star_stalker/server.rb
+++ w03/d04/student/star_stalker/server.rb
@@ -8,17 +8,16 @@ module  Jedi
 
 
     get("/") do
-       "Hello, Welcome to a far, far away Galaxy. This is the place where your Jedi training begins"
-       render (:erb, :index, {:layout => :default_layout})
+       render(:erb, :index, {:layout => :default_layout})
     end
 
 
     get("/about") do
-       render (:erb, :about, {:layout => :default_layout})
+       render(:erb, :about, {:layout => :default_layout})
     end
 
     get("/stars") do
-       render (:erb, :stars, {:layout => :default_layout})
+       render(:erb, :stars, {:layout => :default_layout})
     end
 
 
