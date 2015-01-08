78412e7e6837a41dc77e911cae770a658ccffd31
Patricio Calderon <ourobor@gmail.com>
2015-01-08 07:27:22 -0500

    adds homework

--- w04/d03/student/wdinstagram/server.rb
+++ w04/d03/student/wdinstagram/server.rb
@@ -20,5 +20,28 @@ module Wdinstagram
     get('/') do
       render(:erb, :index, :layout => :default)
     end
-  end
-end
+
+
+    get ('/entries') do
+      @entries = $redis.lrange("entries", 0, -1)
+      render(:erb, :entries, :layout => :default)
+    end
+
+    get ('/entries/new') do
+      render(:erb, :new, :layout => :default)
+      end
+
+    post ('/entries') do
+       id = $redis.incr("user_id")
+       author =   params["author"]
+       photo  =   params["photo_url"]
+       date   =   params["date_taken"]
+       $redis.hmset("user:#{id}", "author", author, "photo_url", photo, "date_taken", date )
+       $redis.lpush("entries", id)
+       redirect to ("/entries")
+       #binding.pry
+    end
+
+
+  end #Class
+end #Module
