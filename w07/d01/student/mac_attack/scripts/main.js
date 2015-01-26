63d1d5a226a23e7bf5826fe5ce1504dd5f2302a3
Patricio Calderon <ourobor@gmail.com>
2015-01-26 14:05:03 -0500

    to pull down

--- w07/d01/student/mac_attack/scripts/main.js
+++ w07/d01/student/mac_attack/scripts/main.js
@@ -6,6 +6,26 @@ var ul = document.createElement("ul");
 p.textContent = "A list of their albums:"
 
 // add content to the DOM
+window.addEventListener("load", function()
+
 var elAlbums = document.querySelector(".albums");
 elAlbums.appendChild(p);
 elAlbums.appendChild(ul);
+
+  )
+
+var inhabitants = document.querySelector("h1");
+
+for(var i = 0, len=fleetwoodMacAlbums.length; i<len; i++) {
+  var li = document.createElement('li');
+  li.setAttribute("class", "album");
+  li.textContent = fleetwoodMacAlbums[i].title + fleetwoodMacAlbums[i].releasedOn;
+  ul.appendChild(li);
+}
+
+
+for (var i = 0, len=fleetwoodMacAlbums.length; i<len; i++) {
+  addLi
+}
+
+
