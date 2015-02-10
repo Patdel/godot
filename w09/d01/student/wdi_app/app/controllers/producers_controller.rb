29ab05d4b6ba7b06742ef99c57c5f427cf6ae75d
Patricio Calderon <ourobor@gmail.com>
2015-02-10 08:59:45 -0500

    adds hw

---
+++ w09/d01/student/wdi_app/app/controllers/producers_controller.rb
@@ -0,0 +1,10 @@
+class ProducersController < ApplicationController
+
+   # a controller action
+  def index #(The methods are called actions)
+    @name = "Sarah"
+    # by default
+    # renders a view called /views/
+    # render
+  end
+end
---
+++ w09/d01/student/wdi_app/app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+
+   # a controller action
+  def index #(The methods are called actions)
+
+    # by default
+    # renders a view called /views/
+    # render
+  end
+end
